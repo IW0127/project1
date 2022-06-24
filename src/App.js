b7fcf7bdf9b5ef3d7d2867d4654a1ea7b744dabc
keyur navadiya <iw0098>
2022-06-24 19:00:31 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
-function App() {
-  const nav = { title: "TextUtils" };
-  return (
-    <>
-      <Navbar nav={nav} />
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
-      </div>
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.nav = { title: "TextUtils" };
+  }
+  render() {
+    return (
+      <>
+        <Router>
+          <Navbar nav={this.nav} />
+          <div className="container my-3">
+            <Switch>
+              <Route path="/home">
+                <TextForm heading="Enter the text to analyze below" />
+              </Route>
+              <Route path="/register">{/* <Register /> */}</Route>
+
+              <Route path="/">{/* <Home /> */}</Route>
+            </Switch>
+          </div>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
